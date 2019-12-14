e6c9320bc861f32bb983880e4f4f54706e436e50
Paulo <paulo.leite@academico.ifpb.edu.br>
2019-12-13 21:39:12 -0300

    Modificacoes no tabuleiro

--- funcoes.h
+++ funcoes.h
@@ -2,6 +2,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define TRUE 1
+#define FALSE 0
+
+#define Ci 1 //Linhas ocupadas pela corveta
+#define Cj 2 //Colunas ocupadas pela corveta
+
+#define Si 1 //Submarino
+#define Sj 3
+
+#define Fi 1 //Fragata
+#define Fj 3 
+
+#define Di 1 //destroyer
+#define Dj 4
+
+#define Pi 6 //Porta-aviões
+#define Pj 6 
+
+#define Bi 1 //Bóia
+#define Bj 1
 
 /* Função para preencher o tabuleiro */
 void preencherTabuleiro(char *tabuleiro);
@@ -9,5 +29,8 @@ void preencherTabuleiro(char *tabuleiro);
 /* Imprimir o tabuleiro */
 void imprimirTabuleiro(char *tabuleiro);
 
-/* Função para gerar os números aleatórios */
+/* Função para posicionar os navios da máquina aleatórios */
+
+
+
 
