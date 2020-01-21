f9fe2a376782ae39d99862dcbb9b3db652e6ba18
francisco <franciscojunior7222@gmail.com>
2020-01-21 10:48:22 -0300

    novas funcoes

--- jogoMemoria.c
+++ jogoMemoria.c
@@ -7,12 +7,14 @@
 #include <conio.h>
 #include <time.h>
 
+#define TAM 6
+
 int main(void){
 	
 	int a = 0,b = 0,acertos = 0;
-	char c;
-	int numeros [5];
-	int respostas;
+	char f;
+	int numeros[6];
+	int respostas[6];
 	
 //inclusao de linguagem
 setlocale(LC_ALL,"Portuguese");
@@ -26,5 +28,15 @@ srand(time(NULL));
 printf("O programa gera 6 numeros aleatorios entre 1 e 25\n");
 printf("Decore os numeros exibidos, e após cinco segundos \nreescreva-os na tela na mesma ordem\n");
 printf("Start Game -[ENTER]");
+
+//corresponde ao enter, com o valor de 13 pela tabela ascii
+do{
+	f = getch();
+}while(f != 13);
+
+for(a = 0; a < TAM ; a++){
+	numeros[a] = (rand() % 25) + 1;
+}
+
 
 }
