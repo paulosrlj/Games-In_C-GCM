9d78a304f80bcd7040e0966855f25f29b93182cd
francisco <franciscojunior7222@gmail.com>
2020-02-13 12:54:34 -0300

    funções de marcio

---
+++ JogoDaVelha.h
@@ -0,0 +1,24 @@
+#define TRUE 1
+#define FALSE 0
+
+//imprimir;
+
+void imprimi(int jogo[3][3]);
+
+// procedimento de jogo;
+
+void jogar(char jogador[50], int jogo[3][3]);
+
+////Limpa a matriz e transformar vencedor em FALSE;
+
+void iniciar(int jogo[3][3]);
+
+//verifica
+
+int verificaD(int jogo[3][3]);
+
+int verificaC(int jogo[3][3]);
+
+int verificaL(int jogo[3][3]);
+
+void menuVelha (int n);
